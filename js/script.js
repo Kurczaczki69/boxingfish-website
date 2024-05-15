450c1dda18484cf4ecf45a4c5e3f722f17b5d4cf
luku_ <98872293+Kurczaczki69@users.noreply.github.com>
2024-05-15 15:14:33 +0200

    added javascript files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+const btn = document.getElementById("copy-ip-btn");
+const btn_txt = document.getElementById("copy-ip-btn-txt");
+const txt = "BoxingFish.pl";
 
+const copyToClip = (txt) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+    btn_txt.innerHTML = "Skopiowano!";
+    return navigator.clipboard.writeText(txt);
+  }
+  return Promise.reject("The Clipboard API is not available.");
+};
+
+btn.addEventListener(
+  "click",
+  function () {
+    copyToClip(txt);
+  },
+  false
+);
